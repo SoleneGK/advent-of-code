c1f00903b40252bbe8355592e32c027d4bd4d0f7
Solene Garda-Krebs <solene.garda.krebs@gmail.com>
2020-12-04 19:55:25 +0100

    2020, day 4, part 2

--- 2020/04/main.go
+++ 2020/04/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -20,8 +22,109 @@ type Passport struct {
 	CountryID      string
 }
 
+// I have done two IsValid methods because i want to keep getting the answer to the first part
 func (p *Passport) IsValidPart1() bool {
-	return p.BirthYear != "" && p.IssueYear != "" && p.ExpirationYear != "" && p.Height != "" && p.HairColor != "" && p.EyeColor != "" && p.PassportID != ""
+	return p.BirthYear != "" &&
+		p.IssueYear != "" &&
+		p.ExpirationYear != "" &&
+		p.Height != "" &&
+		p.HairColor != "" &&
+		p.EyeColor != "" &&
+		p.PassportID != ""
+}
+
+func (p *Passport) IsValidPart2() bool {
+	return p.isValidBirthYear() &&
+		p.isValidIssueYear() &&
+		p.isValidExpirationYear() &&
+		p.isValidHeight() &&
+		p.isValidHairColor() &&
+		p.isValidEyeColor() &&
+		p.isValidPassportID()
+}
+
+func (p *Passport) isValidBirthYear() bool {
+	if p.BirthYear == "" {
+		return false
+	}
+
+	value, err := strconv.Atoi(p.BirthYear)
+
+	return err == nil && value >= 1920 && value <= 2002
+}
+
+func (p *Passport) isValidIssueYear() bool {
+	if p.IssueYear == "" {
+		return false
+	}
+
+	value, err := strconv.Atoi(p.IssueYear)
+
+	return err == nil && value >= 2010 && value <= 2020
+}
+
+func (p *Passport) isValidExpirationYear() bool {
+	if p.ExpirationYear == "" {
+		return false
+	}
+
+	value, err := strconv.Atoi(p.ExpirationYear)
+
+	return err == nil && value >= 2020 && value <= 2030
+}
+
+func (p *Passport) isValidHeight() bool {
+	if p.Height == "" {
+		return false
+	}
+
+	sizeAsString := p.Height[:len(p.Height)-2]
+	size, err := strconv.Atoi(sizeAsString)
+
+	if err != nil {
+		return false
+	}
+
+	unit := p.Height[len(p.Height)-2:]
+
+	if unit == "cm" {
+		return size >= 150 && size <= 193
+	} else {
+		return size >= 59 && size <= 76
+	}
+}
+
+func (p *Passport) isValidHairColor() bool {
+	if p.HairColor == "" {
+		return false
+	}
+
+	pattern := regexp.MustCompile(`^#[0-9a-f]{6}$`)
+
+	return pattern.FindString(p.HairColor) != ""
+}
+func (p *Passport) isValidEyeColor() bool {
+	if p.EyeColor == "" {
+		return false
+	}
+
+	for _, color := range validEyeColor {
+		if color == p.EyeColor {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (p *Passport) isValidPassportID() bool {
+	if p.PassportID == "" {
+		return false
+	}
+
+	_, err := strconv.Atoi(p.PassportID)
+
+	return err == nil && len(p.PassportID) == 9
 }
 
 func (p *Passport) Hydrate(data map[string]string) {
@@ -41,6 +144,8 @@ var correspondance = map[string]string{
 	"cid": "CountryID",
 }
 
+var validEyeColor = []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
 func main() {
 	file, err := os.Open("data")
 	if err != nil {
@@ -49,15 +154,21 @@ func main() {
 	defer file.Close()
 
 	passportList := extractPassportData(file)
-	numberOfValidPassport := 0
+	numberOfValidPassportForPart1 := 0
+	numberOfValidPassportForPart2 := 0
 
 	for _, passport := range passportList {
 		if passport.IsValidPart1() {
-			numberOfValidPassport++
+			numberOfValidPassportForPart1++
+		}
+
+		if passport.IsValidPart2() {
+			numberOfValidPassportForPart2++
 		}
 	}
 
-	fmt.Printf("There are %d valid passport\n", numberOfValidPassport)
+	fmt.Printf("There are %d valid passport with rules for part 1\n", numberOfValidPassportForPart1)
+	fmt.Printf("There are %d valid passport with rules for part 2\n", numberOfValidPassportForPart2)
 }
 
 func extractPassportData(rawData io.Reader) []Passport {
--- 2020/04/main_test.go
+++ 2020/04/main_test.go
@@ -254,3 +254,393 @@ func TestExtractPassportData(t *testing.T) {
 		})
 	}
 }
+
+// This test is a copy-past of TestIsValidPart1
+// I know, DRY, but it's for formal practice
+func TestIsValidPart2(t *testing.T) {
+	testTable := []struct {
+		name     string
+		passport Passport
+		want     bool
+	}{
+		{
+			name: "Valid passport with size in cm",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+			},
+			want: true,
+		},
+		{
+			name: "Valid passport with size in in",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "64in",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+			},
+			want: true,
+		},
+		{
+			name: "Missing BirthYear",
+			passport: Passport{
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing IssueYear",
+			passport: Passport{
+				BirthYear:      "1989",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing ExpirationYear",
+			passport: Passport{
+				BirthYear:  "1989",
+				IssueYear:  "2020",
+				Height:     "164cm",
+				HairColor:  "#ed1c24",
+				EyeColor:   "blu",
+				PassportID: "123456789",
+				CountryID:  "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing Height",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing HairColor",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing EyeColor",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Missing PassportID",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "BirthYear too low",
+			passport: Passport{
+				BirthYear:      "1919",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "BirthYear too high",
+			passport: Passport{
+				BirthYear:      "2003",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "IssueYear too low",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2009",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "IssueYear too high",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2021",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "ExpirationYear too low",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2019",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "ExpirationYear too high",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2031",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Heigh in cm too low",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "149cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Heigh in cm too high",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "194cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Heigh in in too low",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "58in",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Heigh in in too high",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "77in",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect hair color, no #",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect hair color, more than 6 characters after #",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#eed1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect hair color, less than 6 characters after #",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#d1c24",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect hair color, unauthorized characters",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ghijkl",
+				EyeColor:       "blu",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect eye color",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blue",
+				PassportID:     "123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect PassportID, not a number",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "a23456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect PassportID, not enough digits",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "2346789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+		{
+			name: "Incorrect PassportID, too many digits",
+			passport: Passport{
+				BirthYear:      "1989",
+				IssueYear:      "2020",
+				ExpirationYear: "2025",
+				Height:         "164cm",
+				HairColor:      "#ed1c24",
+				EyeColor:       "blu",
+				PassportID:     "1123456789",
+				CountryID:      "98",
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.passport.IsValidPart2()
+
+			if got != tt.want {
+				t.Errorf("Incorrect passport validity: got %t, want %t", got, tt.want)
+			}
+		})
+	}
+}
