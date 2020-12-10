bf0d9202811c8931f27797d2c370f86997e5c93a
Solene Garda-Krebs <solene.garda.krebs@gmail.com>
2020-12-10 18:47:39 +0100

    2020, day 10, part 2

--- 2020/10/main.go
+++ 2020/10/main.go
@@ -18,9 +18,13 @@ func main() {
 
 	adapterList := AdapterList{}
 	adapterList.GetInput(file)
+
 	numberOfDifferences := adapterList.CountDifferences()
-	answer := numberOfDifferences[1] * numberOfDifferences[3]
-	fmt.Printf("There are %d 1-jolt differences and %d 3-jolt differences, the product is %d\n", numberOfDifferences[1], numberOfDifferences[3], answer)
+	answerPart1 := numberOfDifferences[1] * numberOfDifferences[3]
+	fmt.Printf("There are %d 1-jolt differences and %d 3-jolt differences, the product is %d\n", numberOfDifferences[1], numberOfDifferences[3], answerPart1)
+
+	answerPart2 := adapterList.GetCombinaisonNumber()
+	fmt.Printf("There are %d possible combinations\n", answerPart2)
 }
 
 type AdapterList struct {
@@ -38,7 +42,6 @@ func (a *AdapterList) GetInput(file io.Reader) {
 	a.addStartValue()
 	sort.Ints(a.adapters)
 	a.addEndValue()
-	fmt.Printf("%v\n", a.adapters)
 }
 
 func (a *AdapterList) addStartValue() {
@@ -59,4 +62,55 @@ func (a *AdapterList) CountDifferences() map[int]int {
 		numberOfDifferences[difference]++
 	}
 	return numberOfDifferences
+}
+
+/* Algo detail:
+ * I starting with the end: i have 1 possibility for the last adapter
+ * Then i go back: for each adapter, i take the higher adapters it is compatible with
+ * The combination number of this adapter is the sum of the combinations of
+ * the higher adapters
+ * To get the total number of combination, i take the value for the lowest adapter
+ */
+func (a *AdapterList) GetCombinaisonNumber() int {
+	// For each adapter, store the number of possible combination starting with it
+	combinationNumber := a.initializeCombinationNumberMap()
+
+	for i := len(a.adapters) - 2; i >= 0; i-- {
+		currentAdapterRating := a.adapters[i]
+
+		compatibleHigherAdapters := a.getCompatibleHigherAdapters(i)
+
+		for _, higherAdapter := range compatibleHigherAdapters {
+			combinationNumber[currentAdapterRating] += combinationNumber[higherAdapter]
+		}
+	}
+
+	return combinationNumber[0]
+}
+
+func (a *AdapterList) initializeCombinationNumberMap() map[int]int {
+	newMap := map[int]int{}
+
+	for _, adapter := range a.adapters {
+		newMap[adapter] = 0
+	}
+
+	// Value to higest adapter rating starts at 1
+	newMap[a.adapters[len(a.adapters)-1]] = 1
+
+	return newMap
+}
+
+func (a *AdapterList) getCompatibleHigherAdapters(index int) (adapterRatings []int) {
+	maxRating := a.adapters[index] + 3
+
+	for i := index + 1; i < len(a.adapters); i++ {
+		if a.adapters[i] <= maxRating {
+			adapterRatings = append(adapterRatings, a.adapters[i])
+		} else {
+			break
+		}
+	}
+
+	return
 }
