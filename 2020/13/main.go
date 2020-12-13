e2aaa316b0731ea1b633a0cad115d834088166bf
Solene Garda-Krebs <solene.garda.krebs@gmail.com>
2020-12-13 16:30:57 +0100

    2020, day 13, part 2

--- 2020/13/main.go
+++ 2020/13/main.go
@@ -17,9 +17,12 @@ func main() {
 	defer file.Close()
 
 	myTimestamp, busTimetable := ParseFile(file)
+
 	busLine, waitingTime := busTimetable.GetFirstBus(myTimestamp)
 	fmt.Printf("The first bus to come is line n°%d and you wait it for %d minutes. The answer is then %d.\n", busLine, waitingTime, busLine*waitingTime)
 
+	answerPart2 := busTimetable.GetAnswerPart2()
+	fmt.Printf("The answer to part 2 is %d\n", answerPart2)
 }
 
 func ParseFile(file io.Reader) (myTimestamp int, busTimetable BusTimetable) {
@@ -35,16 +38,19 @@ func ParseFile(file io.Reader) (myTimestamp int, busTimetable BusTimetable) {
 }
 
 type BusTimetable struct {
-	Lines []int
+	Lines          []int
+	LinesWithIndex map[int]int
 }
 
 func (b *BusTimetable) SetBusLines(rawData string) {
+	b.LinesWithIndex = map[int]int{}
 	linesList := strings.Split(rawData, ",")
 
-	for _, line := range linesList {
+	for index, line := range linesList {
 		if line != "x" {
 			lineNumber, _ := strconv.Atoi(line)
 			b.Lines = append(b.Lines, lineNumber)
+			b.LinesWithIndex[lineNumber] = index
 		}
 	}
 }
@@ -75,4 +81,50 @@ func getNextBus(waitingTimes map[int]int) int {
 	}
 
 	return fastestLine
+}
+
+func (b *BusTimetable) GetAnswerPart2() int {
+	offsets := b.getOffsets()
+
+	currentStep := b.Lines[0]
+	currentOffset := b.LinesWithIndex[currentStep]
+
+	for _, lineNumber := range b.Lines[1:] {
+		fmt.Printf("working on line n°%d, offset %d\n", lineNumber, offsets[lineNumber])
+		if currentStep > lineNumber {
+			currentStep, currentOffset = addNewConstraint(currentStep, currentOffset, lineNumber, offsets[lineNumber])
+		} else {
+			currentStep, currentOffset = addNewConstraint(lineNumber, offsets[lineNumber], currentStep, currentOffset)
+		}
+	}
+
+	return currentOffset
+}
+
+func (b *BusTimetable) getOffsets() map[int]int {
+	offsets := map[int]int{}
+
+	for _, lineNumber := range b.Lines {
+		offsets[lineNumber] = lineNumber - (b.LinesWithIndex[lineNumber] % lineNumber)
+	}
+
+	return offsets
+}
+
+// Required: step1 > step2
+func addNewConstraint(step1, offset1, step2, offset2 int) (newStep, newOffset int) {
+	i := 0
+
+	for {
+		i++
+
+		bigValue := step1*i + offset1
+		smallValue := ((bigValue-offset2)/step2)*step2 + offset2
+
+		if bigValue == smallValue {
+			break
+		}
+	}
+
+	return step1 * step2, step1*i + offset1
 }
