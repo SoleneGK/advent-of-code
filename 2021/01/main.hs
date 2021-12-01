9b341c53754127af888ee274e059f83b41899288
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2021-12-01 13:47:25 +0100

    2021, day 01, part 2

--- 2021/01/main.hs
+++ 2021/01/main.hs
@@ -5,7 +5,9 @@ main = do
     fileContent <- readFile path
     let values = map read (lines fileContent)
         answerPart1 = (length . getWhenDeeper . pairing) values
+        answerPart2 = getAnswerPart2 values
     putStrLn $ "The answer for part 1 is: " ++ (show answerPart1)
+    putStrLn $ "The answer for part 2 is: " ++ (show answerPart2)
 
 pairing :: [Int] -> [(Int, Int)]
 pairing [] = []
@@ -14,3 +16,15 @@ pairing (x:y:xs) = (x,y) : pairing (y:xs)
 
 getWhenDeeper :: [(Int, Int)] -> [(Int, Int)]
 getWhenDeeper = filter (\(x,y) -> x < y)
+
+makeTuples :: [Int] -> [(Int, Int, Int)]
+makeTuples [] = []
+makeTuples [a] = []
+makeTuples [a, b] = []
+makeTuples (x:y:z:xs) = (x,y,z) : makeTuples (y:z:xs)
+
+sumTuple :: (Int, Int, Int) -> Int
+sumTuple (a, b, c) = a + b + c
+
+getAnswerPart2 :: [Int] -> Int
+getAnswerPart2 = length . getWhenDeeper . pairing . (map sumTuple) . makeTuples
