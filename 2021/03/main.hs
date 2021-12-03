f4235be348f7f59f30380abd782881114438d8c4
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2021-12-03 19:46:28 +0100

    2021, day 03, part 2

--- 2021/03/main.hs
+++ 2021/03/main.hs
@@ -1,5 +1,6 @@
 import System.Environment
-import Data.List
+import Data.List (transpose, partition)
+import Data.Char(digitToInt)
 
 type Column = [Char]
 type BinNumber = [Int]
@@ -7,7 +8,7 @@ type BinNumber = [Int]
 getValues :: FilePath -> IO [Column]
 getValues path = do
     content <- readFile path
-    return $ (transpose . lines) content
+    return $ lines content
 
 sortDigits :: Column -> (Column, Column)
 sortDigits = partition (=='0')
@@ -24,7 +25,7 @@ getRates ((a, b):xs)
 addDigits :: (Int, Int) -> (BinNumber, BinNumber) -> (BinNumber, BinNumber)
 addDigits (d1, d2) (n1, n2) = (d1:n1, d2:n2)
 
--- must be from least to most significant beat
+-- must be from least to most significant bit
 convertBinToDecLSB :: BinNumber -> Int
 convertBinToDecLSB [] = 0
 convertBinToDecLSB [x] = x
@@ -33,14 +34,66 @@ convertBinToDecLSB (x:xs) = x + 2 * convertBinToDecLSB xs
 convertBinToDec :: BinNumber -> Int
 convertBinToDec = convertBinToDecLSB . reverse
 
+--------------------------------------------------------------------
+
+type Line = [Char]
+
+getNthBits :: Int -> [Line] -> Column
+getNthBits i = map (!!i)
+
+-- If as many 1 as 0, return 1
+getMostCommonBit :: Column -> Char 
+getMostCommonBit c
+    | numberOf0 > numberOf1 = '0'
+    | otherwise = '1'
+    where (numberOf0, numberOf1) = (countDigits . sortDigits) c
+
+-- If as many 1 as 0, return 0
+getLeastCommonBit :: Column -> Char
+getLeastCommonBit c
+    | numberOf0 <= numberOf1 = '0'
+    | otherwise = '1'
+    where (numberOf0, numberOf1) = (countDigits . sortDigits) c
+
+filterByBit :: Char -> Int -> [Line] -> [Line]
+filterByBit c i = filter (\l -> l!!i == c)
+
+getOxGenRate :: [Line] -> Int -> Line
+getOxGenRate [] _ = []
+getOxGenRate [l] _ = l
+getOxGenRate ll i = getOxGenRate filteredList (i+1)
+    where filteredList = filterByBit (getMostCommonBit $ getNthBits i ll) i ll
+
+getCo2ScrubRate :: [Line] -> Int -> Line
+getCo2ScrubRate [] _ = []
+getCo2ScrubRate [l] _ = l
+getCo2ScrubRate ll i = getCo2ScrubRate filteredList (i+1)
+    where filteredList = filterByBit (getLeastCommonBit $ getNthBits i ll) i ll
+
+-- must be from least to most significant bit
+convertLineToDecLSB :: Line -> Int
+convertLineToDecLSB [] = 0
+convertLineToDecLSB [c] = digitToInt c
+convertLineToDecLSB (c:cs) = digitToInt c + 2 * convertLineToDecLSB cs
+
+convertLineToDec :: Line -> Int
+convertLineToDec = convertLineToDecLSB . reverse
+
 main :: IO()
 main = do
     (path:_) <- getArgs
     content <- getValues path
-    let (gammaRateBin, omegaRateBin) = getRates $ map (countDigits . sortDigits) content
-    let gammaRate = convertBinToDec gammaRateBin
-    let omegaRate = convertBinToDec omegaRateBin
+
+    let dataPart1 = transpose content
+        (gammaRateBin, omegaRateBin) = getRates $ map (countDigits . sortDigits) dataPart1
+        gammaRate = convertBinToDec gammaRateBin
+        omegaRate = convertBinToDec omegaRateBin
     putStrLn $ "Gamma rate is: " ++ show gammaRate
     putStrLn $ "Omega rate is: " ++ show omegaRate
     putStrLn $ "Answer for part 1 is: " ++ show (gammaRate * omegaRate)
 
+    let oxGenRate = convertLineToDec $ getOxGenRate content 0
+        co2ScrubRate = convertLineToDec $ getCo2ScrubRate content 0
+    putStrLn $ "Oxygen generator rating is: " ++ show oxGenRate
+    putStrLn $ "CO2 scrubber rating is: " ++ show co2ScrubRate
+    putStrLn $ "Answer for part 2 is: " ++ show (oxGenRate * co2ScrubRate)
