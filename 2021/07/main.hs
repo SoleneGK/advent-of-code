20ec75d0f9ac955e51dcfbf2f05958a319a7887c
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2021-12-08 14:12:22 +0100

    2021, day 07, part 2

--- 2021/07/main.hs
+++ 2021/07/main.hs
@@ -38,9 +38,29 @@ getAllFuelCosts i p
 getMinimalFuelCost :: PositionList -> Int
 getMinimalFuelCost l = minimum $ getAllFuelCosts (maxPosition l) l
 
+---------- part 2
+
+getFuelCostForPositionPart2 :: Int -> (Position, Int) -> Int
+getFuelCostForPositionPart2 i (p, nb) = nb * unitCost
+    where distance = abs (p - i)
+          unitCost = distance * (distance + 1) `div` 2
+
+getFuelCostPart2 :: Int -> PositionList -> Int
+getFuelCostPart2 i = sum . map (getFuelCostForPositionPart2 i)
+
+getAllFuelCostsPart2 :: Int -> PositionList -> [Int]
+getAllFuelCostsPart2 i p
+    | i < 0 = []
+    | otherwise = getFuelCostPart2 i p : getAllFuelCostsPart2 (i-1) p
+
+getMinimalFuelCostPart2 :: PositionList -> Int
+getMinimalFuelCostPart2 l = minimum $ getAllFuelCostsPart2 (maxPosition l) l
+
+
 main :: IO ()
 main = do
     (path:_) <- getArgs
     rawData <- getPositions path
     let positions = getPositionList rawData
-    putStrLn $ "The minimal cost of fuel is: " ++ show (getMinimalFuelCost positions)
+    putStrLn $ "The minimal cost of fuel for part 1 is: " ++ show (getMinimalFuelCost positions)
+    putStrLn $ "The minimal cost of fuel for part 2 is: " ++ show (getMinimalFuelCostPart2 positions)
