d2f69ff92e203a2f92ecc28c9f030ba553604e46
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-12-01 17:19:16 +0100

    2015, day 02, part 2

--- 2015/02/main.php
+++ 2015/02/main.php
@@ -3,30 +3,45 @@
 $presentList = fopen('input.txt', 'rb');
 
 $paperNeeded = 0;
+$ribbonNeeded = 0;
 
 while (false !== $present = fgets($presentList)) {
     $presentDimensions = explode('x', $present);
     // 0 => l, 1 => w, 2 => h
 
+    // Partie 1
+
     // Surfaces
-    $presentPaperAreas = [
+    $presentAreas = [
         $presentDimensions[0] * $presentDimensions[1],
         $presentDimensions[1] * $presentDimensions[2],
         $presentDimensions[0] * $presentDimensions[2]
     ];
 
     // Supplément = plus petite surface
-    $paperSlack = min($presentPaperAreas);
+    $paperSlack = min($presentAreas);
 
     // Total du papier nécessaire
-    $paperSize =
-        2 * $presentPaperAreas[0]
-        + 2 * $presentPaperAreas[1]
-        + 2 * $presentPaperAreas[2]
+    $paperNeeded +=
+        2 * $presentAreas[0]
+        + 2 * $presentAreas[1]
+        + 2 * $presentAreas[2]
         + $paperSlack
     ;
 
-    $paperNeeded += $paperSize;
+    // Partie 2
+
+    // Périmètres
+    $presentPerimeters = [
+        2 * $presentDimensions[0] + 2 * $presentDimensions[1],
+        2 * $presentDimensions[1] + 2 * $presentDimensions[2],
+        2 * $presentDimensions[0] + 2 * $presentDimensions[2]
+    ];
+
+    $presentVolume = $presentDimensions[0] * $presentDimensions[1] * $presentDimensions[2];
+
+    $ribbonNeeded += min($presentPerimeters) + $presentVolume;
+
 }
 
-echo "They should order $paperNeeded square feet of wrapping paper\n";
+echo "They should order $paperNeeded square feet of wrapping paper and $ribbonNeeded feet of ribbon\n";
