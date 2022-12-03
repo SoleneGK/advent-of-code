0894748a3d701b6c12184fa4adb42a5350d228d1
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-12-03 13:31:01 +0100

    2022, day 03, part 2

--- 2022/03/main.php
+++ 2022/03/main.php
@@ -4,6 +4,14 @@ $file = fopen('input.txt', 'rb');
 
 $prioritySum = 0;
 
+function getPriority(string $char): int
+{
+    $isCapital = ctype_upper($char);
+    $asciiValue = ord($char);
+
+    return $isCapital ? $asciiValue - 64 + 26 : $asciiValue - 96;
+}
+
 while (false !== $rucksack = fgets($file)) {
     $rucksack = str_split($rucksack);
 
@@ -12,10 +20,29 @@ while (false !== $rucksack = fgets($file)) {
     $errorItemList = array_intersect($firstHalf, $secondHalf);
     $errorItem = array_shift($errorItemList);
 
-    $isCapital = ctype_upper($errorItem);
-    $asciiValue = ord($errorItem);
-
-    $prioritySum += $isCapital ? $asciiValue - 64 + 26 : $asciiValue - 96;
+    $prioritySum += getPriority($errorItem);
 }
 
 echo "The answer for part 1 is $prioritySum\n";
+
+rewind($file);
+
+$prioritySum = 0;
+
+while (
+    (false !== $rucksack[0] = fgets($file))
+    && (false !== $rucksack[1] = fgets($file))
+    && (false !== $rucksack[2] = fgets($file))
+) {
+    array_walk($rucksack, static function (&$value) {
+        $value = str_split(trim($value));
+    });
+
+    /** @var array $commonItemList */
+    $commonItemList = array_intersect(...$rucksack);
+    $commonItem = array_shift($commonItemList);
+
+    $prioritySum += getPriority($commonItem);
+}
+
+echo "The answer for part 2 is $prioritySum\n";
