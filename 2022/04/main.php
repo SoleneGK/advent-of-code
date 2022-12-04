e7775e82af88f6a113dcf693b8ec687897409600
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-12-04 13:49:03 +0100

    2022, day 04, part 2

--- 2022/04/main.php
+++ 2022/04/main.php
@@ -3,6 +3,7 @@
 $file = fopen('input.txt', 'rb');
 
 $containNumber = 0;
+$overlapNumber = 0;
 
 while (false !== $line = fgets($file)) {
     [$elf1Assignment, $elf2Assignment] = explode(',', trim($line));
@@ -12,15 +13,39 @@ while (false !== $line = fgets($file)) {
 
     if (
         (
+            // 1 contains 2
             $elf1AssignmentStart <= $elf2AssignmentStart
             && $elf1AssignmentEnd >= $elf2AssignmentEnd
         ) || (
+            // 2 contains 1
             $elf1AssignmentStart >= $elf2AssignmentStart
             && $elf1AssignmentEnd <= $elf2AssignmentEnd
         )
     ) {
         ++$containNumber;
+        ++$overlapNumber;
+
+        continue;
+    }
+
+    if (
+        (
+            // Start for 1 is included in assignment for 2
+            $elf1AssignmentStart >= $elf2AssignmentStart
+            && $elf1AssignmentStart <= $elf2AssignmentEnd
+        ) || (
+            // End for 1 is included in assignment for 2
+            $elf1AssignmentEnd >= $elf2AssignmentStart
+            && $elf1AssignmentEnd <= $elf2AssignmentEnd
+        )
+        // No need to check the same with 1 and 2 inverted
+        // If 1 overlaps 2, then 2 overlaps 1
+        // Only case not handled is when 2 is contained in 1
+        // This case is cheched before
+    ) {
+        ++$overlapNumber;
     }
 }
 
 echo "The answer for part 1 is $containNumber\n";
+echo "The answer for part 2 if $overlapNumber\n";
