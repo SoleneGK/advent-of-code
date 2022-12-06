de9b0a6c6a4ab07a069a3a4dc52fa4160aae63d1
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-12-06 14:22:03 +0100

    2015, day 05, part 1

---
+++ 2015/05/main.php
@@ -0,0 +1,50 @@
+<?php
+
+function isNice(string $string): bool
+{
+    // Does the string contains a forbidden string?
+    if (
+        str_contains($string, 'ab')
+        || str_contains($string, 'cd')
+        || str_contains($string, 'pq')
+        || str_contains($string, 'xy')
+    ) {
+        return false;
+    }
+
+    // Does the string contains at least three vowels?
+    $numberOfVowels = substr_count($string, 'a')
+         + substr_count($string, 'e')
+         + substr_count($string, 'i')
+         + substr_count($string, 'o')
+         + substr_count($string, 'u')
+    ;
+
+    if ($numberOfVowels < 3) {
+        return false;
+    }
+
+    // Does the string contains a double letter?
+    $stringAsArray = str_split($string);
+    $iMax = count($stringAsArray) - 1;
+
+    for ($i = 0; $i < $iMax; ++$i) {
+        if ($string[$i] === $string[$i+1]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+$stringList = fopen('input.txt', 'rb');
+
+$numberOfNiceStrings = 0;
+
+while (false !== $string = fgets($stringList)) {
+    if (isNice($string)) {
+        ++$numberOfNiceStrings;
+    }
+}
+
+echo "There are $numberOfNiceStrings nice strings\n";
