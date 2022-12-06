79d8bde6439e817b6f9db7ec130086494ac7807d
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-12-06 14:36:03 +0100

    2015, day 05, part 2

--- 2015/05/main.php
+++ 2015/05/main.php
@@ -1,6 +1,6 @@
 <?php
 
-function isNice(string $string): bool
+function isNicePart1(string $string): bool
 {
     // Does the string contains a forbidden string?
     if (
@@ -36,14 +36,56 @@ function isNice(string $string): bool
     return false;
 }
 
+function isNicePart2(string $string): bool
+{
+    return hasOverlappingPair($string) && hasOneLetterRepeatingWithOneLetterBetween($string);
+}
+
+function hasOverlappingPair(string $string): bool
+{
+    // I could go with minus 1, but when there are 3 or less character left, it's not possible
+    // to have 2 pairs not overlapping
+    $iMax = strlen($string) - 3;
+
+    for ($i = 0; $i < $iMax; ++$i) {
+        $pair = substr($string, $i, 2);
+
+        // Can we find the pair in the rest of the string?
+        if (str_contains(substr($string, $i+2), $pair)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function hasOneLetterRepeatingWithOneLetterBetween(string $string): bool
+{
+    $iMax = strlen($string) - 2;
+
+    for ($i = 0; $i < $iMax; ++$i) {
+        if ($string[$i] === $string[$i+2]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 $stringList = fopen('input.txt', 'rb');
 
-$numberOfNiceStrings = 0;
+$numberOfNiceStringsPart1 = 0;
+$numberOfNiceStringsPart2 = 0;
 
 while (false !== $string = fgets($stringList)) {
-    if (isNice($string)) {
-        ++$numberOfNiceStrings;
+    if (isNicePart1($string)) {
+        ++$numberOfNiceStringsPart1;
+    }
+
+    if (isNicePart2($string)) {
+        ++$numberOfNiceStringsPart2;
     }
 }
 
-echo "There are $numberOfNiceStrings nice strings\n";
+echo "There are $numberOfNiceStringsPart1 nice strings with part 1 rules\n";
+echo "There are $numberOfNiceStringsPart2 nice strings with part 2 rules\n";
