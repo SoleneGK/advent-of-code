2c8dfe81e2d73a344d1935041ef2468d54c8f703
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2022-12-10 13:49:17 +0100

    2022, day 10, part 2

--- 2022/10/main.php
+++ 2022/10/main.php
@@ -8,6 +8,13 @@ class CPU
     private int $register = 1;
     private array $interestingSignalStrengths = [];
 
+    private CRT $crt;
+
+    public function __construct(CRT $crt)
+    {
+        $this->crt = $crt;
+    }
+
     public function getInterestingSignalStrengths(): array
     {
         return $this->interestingSignalStrengths;
@@ -21,6 +28,7 @@ class CPU
     public function incrementClock(): void
     {
         ++$this->clock;
+        $this->crt->drawPixel($this->register);
 
         if (in_array($this->clock, self::INTERESTING_CYCLES)) {
             $this->interestingSignalStrengths[$this->clock] = $this->getSignalStrength();
@@ -43,9 +51,51 @@ class CPU
     }
 }
 
+class CRT
+{
+    private const LIT_PIXEL = '#';
+    private const DARK_PIXEL = '.';
+
+    private int $currentPixel = 0;
+    private array $screen = [];
+
+    private function setNextPixel(): void
+    {
+        $this->currentPixel = ++$this->currentPixel % 40;
+    }
+
+    public function drawPixel(int $spriteMiddlePosition): void
+    {
+        if (
+            $this->currentPixel >= $spriteMiddlePosition - 1
+            && $this->currentPixel <= $spriteMiddlePosition + 1
+        ) {
+            $this->screen[] = self::LIT_PIXEL;
+        } else {
+            $this->screen[] = self::DARK_PIXEL;
+        }
+
+        $this->setNextPixel();
+    }
+
+    public function display(): void
+    {
+        foreach ($this->screen as $key => $pixel) {
+            if (0 === $key % 40) {
+                echo "\n";
+            }
+
+            echo $pixel;
+        }
+
+        echo "\n";
+    }
+}
+
 $instructionList = fopen('input.txt', 'rb');
 
-$cpu = new CPU();
+$crt = new CRT();
+$cpu = new CPU($crt);
 
 while (false !== $instruction = fgets($instructionList)) {
     $cpu->executeInstruction($instruction);
@@ -54,3 +104,5 @@ while (false !== $instruction = fgets($instructionList)) {
 $answerPart1 = array_sum($cpu->getInterestingSignalStrengths());
 
 echo "The answer to part 1 is $answerPart1\n";
+
+$crt->display();
