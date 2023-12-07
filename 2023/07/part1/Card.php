998c6b23839d3e2aaf9845e2c865da282814dd44
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2023-12-07 11:00:16 +0100

    20232, day 07, part 1

---
+++ 2023/07/part1/Card.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+enum Card: int
+{
+    case A = 14;
+    case K = 13;
+    case Q = 12;
+    case J = 11;
+    case T = 10;
+    case NINE = 9;
+    case EIGHT = 8;
+    case SEVEN = 7;
+    case SIX = 6;
+    case FIVE = 5;
+    case FOUR = 4;
+    case THREE = 3;
+    case TWO = 2;
+
+    public static function getCard(string $cardValue): Card
+    {
+        if (1 !== strlen($cardValue)) {
+            throw new InvalidArgumentException();
+        }
+
+        return match ($cardValue) {
+            'A' => self::A,
+            'K' => self::K,
+            'Q' => self::Q,
+            'J' => self::J,
+            'T' => self::T,
+            '9' => self::NINE,
+            '8' => self::EIGHT,
+            '7' => self::SEVEN,
+            '6' => self::SIX,
+            '5' => self::FIVE,
+            '4' => self::FOUR,
+            '3' => self::THREE,
+            '2' => self::TWO,
+            default => throw new InvalidArgumentException(),
+        };
+    }
+}
