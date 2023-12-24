9bb5c30812633fb09b2ddda1229b8f3a01256bee
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2023-12-24 17:56:06 +0100

    2018, day 01, part 2

--- 2018/01/main.php
+++ 2018/01/main.php
@@ -13,3 +13,27 @@ while ($line = fgets($file)) {
 fclose($file);
 
 echo "The resulting frequency is $frequency\n";
+
+
+$frequencyChangeList = array_map('intval', explode(PHP_EOL, file_get_contents('input.txt')))  ;
+
+$frequency = 0;
+$reachedFrenquencyList = [0];
+
+$i = 0;
+$iMax = count($frequencyChangeList);
+$answerFound = false;
+
+while (!$answerFound) {
+    $frequency += $frequencyChangeList[$i];
+
+    if (\in_array($frequency, $reachedFrenquencyList, true)) {
+        $answerFound = true;
+    }
+
+    $reachedFrenquencyList[] = $frequency;
+
+    $i = ++$i % $iMax;
+}
+
+echo "The first frequency the device reach twice is $frequency\n";
