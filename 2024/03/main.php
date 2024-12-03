3d2c5f83453c4c83973072c00ec8b3e2224438fa
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2024-12-03 20:44:34 +0100

    2024, day 03, part 2

--- 2024/03/main.php
+++ 2024/03/main.php
@@ -4,21 +4,65 @@ declare(strict_types=1);
 
 $file = fopen('input.txt', 'rb');
 
-$total = 0;
+$data = [];
 
 while (false !== $line = fgets($file)) {
-    $line = trim($line);
+    $data[] = trim($line);
+}
+
+
+$total = 0;
+
+foreach ($data as $line) {
     $matchList = [];
 
     preg_match_all('/mul\(\d{1,3},\d{1,3}\)/', $line, $matchList);
 
     foreach ($matchList[0] as $match) {
-        $a = substr($match, 4);
-        $a = substr($a, 0, strlen($a) - 1);
-
-        $a = explode(',', $a);
-        $total += (int) $a[0] * (int) $a[1];
+        $total += getMultResult($match);
     }
 }
 
+function getMultResult(string $mult) {
+    $a = substr($mult, 4);
+    $a = substr($a, 0, strlen($a) - 1);
+
+    $a = explode(',', $a);
+    return (int) $a[0] * (int) $a[1];
+}
+
 echo "Total: $total\n";
+
+
+// Part 2
+
+$total = 0;
+$activated = true;
+
+foreach ($data as $line) {
+    $matchList = [];
+
+    preg_match_all("/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/", $line, $matchList);    
+    
+    foreach ($matchList[0] as $match) {
+        if ($match === 'do()') {
+            $activated = true;
+            
+            continue;
+        }
+    
+        if ($match === "don't()") {
+            $activated = false;
+    
+            continue;
+        }
+    
+        if (!$activated) {
+            continue;
+        }
+    
+        $total += getMultResult($match);
+    }
+}
+
+echo "Total part 2: $total\n";
