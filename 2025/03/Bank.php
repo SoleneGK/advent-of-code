d28dde7778db05b579673b933d8b7812aa4ba2a4
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2025-12-03 15:18:34 +0100

    2025, day 03, part 2

--- 2025/03/Bank.php
+++ 2025/03/Bank.php
@@ -11,7 +11,7 @@ class Bank
         $this->batteryList = str_split($line);
     }
 
-    public function getMaxJoltage(): int
+    public function getMaxJoltageForPart1(): int
     {
         $maxJoltage = 0;
         $numberOfBatteries = count($this->batteryList);
@@ -27,5 +27,77 @@ class Bank
         }
 
         return $maxJoltage;
+    }
+
+    public function getMaxJoltageForPart2(): int
+    {
+        $solution = $this->findBatteriesToActivate(12, -1);
+        $maxJoltage = 0;
+
+        foreach ($solution as $digit) {
+            $maxJoltage *= 10;
+            $maxJoltage += $digit;
+        }
+
+        return $maxJoltage;
+    }
+
+    private function findBatteriesToActivate(int $numberOfBatteriesToActivate, int $firstIndex): array
+    {
+        if ($numberOfBatteriesToActivate <= 0) {
+            return [];
+        }
+
+        // On ne teste pas les éléments avant le 1er index (qui correspond à la position de la batterie activée précédente)
+        // On ne teste pas les $numberOfBatteriesToActivate - 1 derniers éléments, ils ne sont pas des solutions valables
+        $length = count($this->batteryList) - $numberOfBatteriesToActivate - $firstIndex;
+        $batteriesToTest = array_slice($this->batteryList, $firstIndex + 1, $length);
+
+        $newBatteryValue = max($batteriesToTest);
+
+        if ($numberOfBatteriesToActivate === 1) {
+            return [$newBatteryValue];
+        }
+
+        // Renvoie toutes les batteries avec la valeur max
+        $newBatteryIndexList = array_keys($batteriesToTest, $newBatteryValue);
+
+        // Tester pour toutes les solutions
+        $potentialSolutionList = [];
+
+        foreach ($newBatteryIndexList as $newBatteryIndex) {
+            $index = $firstIndex + $newBatteryIndex + 1;
+
+            $potentialSolutionList[] = array_merge(
+                [$newBatteryValue],
+                $this->findBatteriesToActivate($numberOfBatteriesToActivate - 1, $index)
+            );
+        }
+
+        // Trouver la meilleure solution et la renvoyer
+        return $this->getBestSolution($potentialSolutionList);
+    }
+
+    private function getBestSolution(array $solutionList): array
+    {
+        $bestSolution = [];
+
+        foreach ($solutionList as $solution) {
+            if ([] === $bestSolution) {
+                $bestSolution = $solution;
+            }
+
+            $length = \count($solution);
+
+            for ($i = 0; $i < $length; ++$i) {
+                if ($bestSolution[$i] < $solution[$i]) {
+                    $bestSolution = $solution;
+
+                    break;
+                }
+            }
+        }
+
+        return $bestSolution;
     }
 }
--- 2025/03/main.php
+++ 2025/03/main.php
@@ -7,13 +7,16 @@ require_once 'Bank.php';
 $file = fopen('input.txt', 'rb');
 
 $bankList = [];
-$totalOutputJoltage = 0;
+$totalOutputJoltageForPart1 = 0;
+$totalOutputJoltageForPart2 = 0;
 
 while (false !== $line = fgets($file)) {
     $bank = new Bank(trim($line));
     $bankList[] = $bank;
-    $totalOutputJoltage += $bank->getMaxJoltage();
+    $totalOutputJoltageForPart1 += $bank->getMaxJoltageForPart1();
+    $totalOutputJoltageForPart2 += $bank->getMaxJoltageForPart2();
 }
 
-echo "The total output joltage is $totalOutputJoltage\n";
+echo "The total output joltage for part 1 is $totalOutputJoltageForPart1\n";
+echo "The total output joltage for part 2 is $totalOutputJoltageForPart2\n";
 
